6b674d84d7dcefbf147744bcea9d7d1e51935f54
fabrynet <fabrynet.bianco@gmail.com>
2020-07-04 13:10:08 +0200

    Ex. finale con click sui bottoni della nav

--- main.js
+++ main.js
@@ -3,42 +3,69 @@
 
 // eseguo il codice JS al caricamento della pagina
 $(document).ready(function() {
-  nextImg();
-  prevImg();
+  var nextButton = $('.next');
+  var prevButton = $('.prev');
+  var navButton = $('.nav i');
+
+  nextButton.click(nextImg);
+  prevButton.click(prevImg);
+  navButton.click(dotNav);
+  //keydown prende il codice di tutti i tasti della tastiera
+  $(document).keydown(keyPrevNext);
 });
 
 // FUNCTIONS
 function nextImg() {
-  $('.next').click(function() {
-    var imgActive = $('.slider-wrapper img.active');
-    var iActive = $('.slider-wrapper i.active');
-    imgActive.removeClass('active');
-    iActive.removeClass('active');
-    imgFirst = $('.slider-wrapper img.first');
-    iFirst = $('.slider-wrapper i.first');
-    if (imgActive.hasClass('last')) {
-      imgFirst.addClass('active');
-      iFirst.addClass('active');
-    } else {
-      imgActive.next().addClass('active');
-      iActive.next().addClass('active');
-    }
-  });
+  var imgActive = $('.slider-wrapper img.active');
+  var iActive = $('.slider-wrapper i.active');
+
+  imgActive.removeClass('active');
+  iActive.removeClass('active');
+  imgFirst = $('.slider-wrapper img.first');
+  iFirst = $('.slider-wrapper i.first');
+  if (imgActive.hasClass('last')) {
+    imgFirst.addClass('active');
+    iFirst.addClass('active');
+  } else {
+    imgActive.next().addClass('active');
+    iActive.next().addClass('active');
+  }
 }
 function prevImg() {
-  $('.prev').click(function() {
-    var imgActive = $('.slider-wrapper img.active');
-    var iActive = $('.slider-wrapper i.active');
-    imgActive.removeClass('active');
-    iActive.removeClass('active');
-    imgLast = $('.slider-wrapper img.last');
-    iLast = $('.slider-wrapper i.last');
-    if (imgActive.hasClass('first')) {
-      imgLast.addClass('active');
-      iLast.addClass('active');
-    } else {
-      imgActive.prev().addClass('active');
-      iActive.prev().addClass('active');
-    }
-  });
+  var imgActive = $('.slider-wrapper img.active');
+  var iActive = $('.slider-wrapper i.active');
+
+  imgActive.removeClass('active');
+  iActive.removeClass('active');
+  imgLast = $('.slider-wrapper img.last');
+  iLast = $('.slider-wrapper i.last');
+  if (imgActive.hasClass('first')) {
+    imgLast.addClass('active');
+    iLast.addClass('active');
+  } else {
+    imgActive.prev().addClass('active');
+    iActive.prev().addClass('active');
+  }
+}
+function dotNav() {
+  var dot = $('.nav i');
+  var img = $('.slider-wrapper img');
+  var position = $(this).index();
+
+  dot.removeClass('active');
+  $(this).addClass('active');
+  img.removeClass('active');
+  $(img.get(position)).addClass('active');
+}
+function keyPrevNext() {
+  //keyCode e which per supporto cross browser
+  var keyWhich = event.which;
+  var keyCode = event.keyCode;
+  console.log(event.which);
+  console.log(event.keyCode);
+  if (keyWhich == 39 || keyCode == 39) {
+    nextImg();
+  } else if (keyWhich == 37 || keyCode == 37) {
+    prevImg();
+  }
 }
